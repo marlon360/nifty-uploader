f5e9a6ca1ba68f2d8359d0f91df92d5ff788005a
Marlon Lückert <m.lueckert@me.com>
2018-12-20 00:49:26 +0100

    test: mock xhr error and timeout

--- __tests__/mocks/mockXHR.ts
+++ __tests__/mocks/mockXHR.ts
@@ -1,9 +1,17 @@
-export const createMockXHR = (status: number = 200) => {
+export const createMockXHR = (status: number = 200, error: boolean = false, timeout: boolean = false) => {
     const mockXHR = {
         onload: jest.fn(),
+        onerror: jest.fn(),
+        ontimeout: jest.fn(),
         open: jest.fn(),
         send: jest.fn(function () {
-            this.onload()
+            if (error) {
+                this.onerror();
+            } else if(timeout) {
+                this.ontimeout();
+            } else {
+                this.onload();
+            }
         }),
         status:  status
     }
