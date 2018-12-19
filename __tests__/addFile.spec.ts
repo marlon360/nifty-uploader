2ea61ebf1893fbaaba024bc124a803397ad988c7
Marlon Lückert <m.lueckert@me.com>
2018-12-19 16:57:52 +0100

    test: add multiple files

--- __tests__/addFile.spec.ts
+++ __tests__/addFile.spec.ts
@@ -1,14 +1,29 @@
 import { NiftyUploader } from '../src/NiftyUploader';
 
-test('add file to uploader', () => {
+test('add single file to uploader', () => {
     // new uploader instance
     const uploader = new NiftyUploader({
-        endpoint: "endpoint"
+        endpoint: "endpoint",
+        autoUpload: false
     });
     // file length should be 0, because no files were added
     expect(uploader.files.length).toBe(0);
     // add a test file
-    uploader.addFiles([new File([], "testfile")]);
+    uploader.addFile(new File([], "testfile"));
     // length should mow be 1
     expect(uploader.files.length).toBe(1);
+});
+
+test('add multiple file to uploader', () => {
+    // new uploader instance
+    const uploader = new NiftyUploader({
+        endpoint: "endpoint",
+        autoUpload: false
+    });
+    // file length should be 0, because no files were added
+    expect(uploader.files.length).toBe(0);
+    // add a test file
+    uploader.addFiles([new File([], "testfile"), new File([], "testfile2")]);
+    // length should mow be 1
+    expect(uploader.files.length).toBe(2);
 });
