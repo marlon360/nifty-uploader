1d4a3d762a40a4a836c2ce4bba8dcd2c98dd928e
Marlon Lückert <m.lueckert@me.com>
2018-12-20 01:21:13 +0100

    test: file upload start event

---
+++ __tests__/uploadStartedEvent.spec.ts
@@ -0,0 +1,22 @@
+import { createMockXHR } from "./mocks/mockXHR";
+import { NiftyUploader } from "../src/NiftyUploader";
+
+test('file upload start event should be fired', (done) => {
+
+    const mockXHR = createMockXHR();
+    (<any>window).XMLHttpRequest = jest.fn(() => mockXHR);
+
+    // new uploader instance
+    const uploader = new NiftyUploader({
+        chunking: false
+    });
+    const file = new File(["content"], "filename");
+
+    uploader.onFileUploadStarted((data) => {
+        expect.anything();
+        done();
+    });
+
+    uploader.addFiles([file, file, file]);
+    
+});
