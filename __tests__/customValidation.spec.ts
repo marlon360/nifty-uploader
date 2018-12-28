55bc25376d0d904740494009a666cbf0d9b08531
Marlon Lückert <m.lueckert@me.com>
2018-12-28 15:17:26 +0100

    test: custom validator

---
+++ __tests__/customValidation.spec.ts
@@ -0,0 +1,99 @@
+import { NiftyUploader } from '../src/NiftyUploader';
+
+test('custom validation: promise resolved', (done) => {
+    // new uploader instance without options
+    const uploader = new NiftyUploader({
+        autoUpload: false,
+        customValidation: ((file) => {
+            return new Promise((resolve, reject) => {
+                resolve();
+            })
+        })
+    });
+
+    const file = new File(["content"], "filename.jpg");
+
+    uploader.onFileQueued((data) => {
+        expect(data.file.name).toBe(file.name);
+        done();
+    });
+
+    uploader.addFile(file);
+});
+
+test('custom validation: promise resolved with false', (done) => {
+    // new uploader instance without options
+    const uploader = new NiftyUploader({
+        autoUpload: false,
+        customValidation: ((file) => {
+            return new Promise((resolve, reject) => {
+                resolve(false);
+            })
+        })
+    });
+
+    const file = new File(["content"], "filename.jpg");
+
+    uploader.onFileProcessingFailed((data) => {
+        expect(data.file.name).toBe(file.name);
+        done();
+    });
+
+    uploader.addFile(file);
+});
+
+test('custom validation: promise rejected', (done) => {
+    // new uploader instance without options
+    const uploader = new NiftyUploader({
+        autoUpload: false,
+        customValidation: ((file) => {
+            return new Promise((resolve, reject) => {
+                reject();
+            })
+        })
+    });
+
+    const file = new File(["content"], "filename.jpg");
+
+    uploader.onFileProcessingFailed((data) => {
+        expect(data.file.name).toBe(file.name);
+        done();
+    });
+
+    uploader.addFile(file);
+});
+
+test('custom validation: boolean', (done) => {
+    // new uploader instance without options
+    const uploader = new NiftyUploader({
+        autoUpload: false,
+        customValidation: ((file) => {
+            return true;
+        })
+    });
+
+    const file = new File(["content"], "filename.jpg");
+
+    uploader.onFileQueued((data) => {
+        expect(data.file.name).toBe(file.name);
+        done();
+    });
+
+    uploader.addFile(file);
+});
+
+test('custom validation: no validation', (done) => {
+    // new uploader instance without options
+    const uploader = new NiftyUploader({
+        autoUpload: false
+    });
+
+    const file = new File(["content"], "filename.jpg");
+
+    uploader.onFileQueued((data) => {
+        expect(data.file.name).toBe(file.name);
+        done();
+    });
+
+    uploader.addFile(file);
+});
